c3796d84bbdaf2b0498ac4416a2ab6d38b5b3fd9
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-07-07 13:07:18 +0430

    update database

--- sarafi/settings.py
+++ sarafi/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ROOT = 'http://127.0.0.1:8000'
+ROOT = 'http://138.201.116.39'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ra$$l%4d2y4i!5#=(n58_u988+gdn8$st2^rtx-l0buiwhi7kc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['138.201.116.39']
 
 
 # Application definition
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080"
+    "http://138.201.116.39"
 ]
 
 MIDDLEWARE = [
