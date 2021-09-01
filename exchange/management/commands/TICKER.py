7f6dfde94e03b831788da31d18c08b4743dd1673
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-09-01 14:25:53 +0430

    update

--- exchange/management/commands/TICKER.py
+++ exchange/management/commands/TICKER.py
@@ -9,10 +9,10 @@ class Command(BaseCommand):
         r = requests.get(url = 'https://api.nomics.com/v1/currencies/ticker?key=5f176269caf5ea0dfab684904f9316bf1f4f2bc6&ids=BTC,ETH,TRX,DOGE,USDT')
         r = r.json()
         price = Price.objects.get(id = 1)
-        price.btc = r[0]['price'] * 1.07
-        price.eth = r[1]['price'] * 1.07
-        price.usdt = r[2]['price'] * 1.07
-        price.doge = r[3]['price'] * 1.07
-        price.trx = r[4]['price'] * 1.07
+        price.btc = float(r[0]['price']) * 1.07
+        price.eth = float(r[1]['price']) * 1.07
+        price.usdt = float(r[2]['price']) * 1.07
+        price.doge = float(r[3]['price']) * 1.07
+        price.trx = float(r[4]['price']) * 1.07
         price.usd = 250000
         price.save()
