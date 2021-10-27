c0baf2bae15a4cf621781e8b1620b78609d93e2e
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-10-27 13:55:46 +0330

    update

--- exchange/management/commands/leverages.py
+++ exchange/management/commands/leverages.py
@@ -10,20 +10,26 @@ class Command(BaseCommand):
         Lev = Leverage.objects.all()
         for item in Lev:
             if 'USDT' in item.symbol :
-                coinex = CoinEx('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7' )
-                coin = coinex.margin_config(market =  item.symbol)
-                item.leverage = (coin['leverage'])
-                item.buymin = (coin[item.symbol.replace('USDT' , '')]['min_amount'])
-                item.buymax = (coin[item.symbol.replace('USDT' , '')]['max_amount'])
-                item.sellmin =(coin['USDT']['min_amount'])
-                item.sellmax =(coin['USDT']['max_amount'])
+                try:
+                    coinex = CoinEx('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7' )
+                    coin = coinex.margin_config(market =  item.symbol)
+                    item.leverage = (coin['leverage'])
+                    item.buymin = (coin[item.symbol.replace('USDT' , '')]['min_amount'])
+                    item.buymax = (coin[item.symbol.replace('USDT' , '')]['max_amount'])
+                    item.sellmin =(coin['USDT']['min_amount'])
+                    item.sellmax =(coin['USDT']['max_amount'])
+                except:
+                    print('error')
                 item.save()
             elif 'BTC' in item.symbol :
-                coinex = CoinEx('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7' )
-                coin = coinex.margin_config(market =  item.symbol)
-                item.leverage = (coin['leverage'])
-                item.buymin = (coin[item.symbol.replace('BTC' , '')]['min_amount'])
-                item.buymax = (coin[item.symbol.replace('BTC' , '')]['max_amount'])
-                item.sellmin =(coin['BTC']['min_amount'])
-                item.sellmax =(coin['BTC']['max_amount'])
-                item.save()
+                try:
+                    coinex = CoinEx('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7' )
+                    coin = coinex.margin_config(market =  item.symbol)
+                    item.leverage = (coin['leverage'])
+                    item.buymin = (coin[item.symbol.replace('BTC' , '')]['min_amount'])
+                    item.buymax = (coin[item.symbol.replace('BTC' , '')]['max_amount'])
+                    item.sellmin =(coin['BTC']['min_amount'])
+                    item.sellmax =(coin['BTC']['max_amount'])
+                    item.save()
+                except:
+                    print('error')
