22f2d8eef7b3b94adaa177bd0ff80917dfac460d
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-01 13:59:14 +0330

    update

---
+++ exchange/management/commands/RIALTICKER.py
@@ -0,0 +1,13 @@
+from exchange.views import currency
+from exchange.models import Price , Staff,  UserInfo , Currencies , Wallet , Verify , BankCards, Transactions, Settings, Subjects, Tickets, Pages , Forgetrequest
+from django.core.management.base import BaseCommand, CommandError
+import requests
+import time
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        rial = requests.get(url = 'http://api.navasan.tech/latest/?api_key=1oFGrKOHDblbSXNvErx6Y2XIqMghp2h9')   
+        r = rial.json()
+        price = Price.objects.get(id = 1)
+        price.usd = r['usd_buy']['value']
+        price.save()
