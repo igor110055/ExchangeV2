94300f4f13b8955be8e3459a95466b7cca61e2eb
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-05 02:27:40 +0330

    update

--- exchange/views.py
+++ exchange/views.py
@@ -102,7 +102,7 @@ class timeout(APIView):
     def get(self, request, format=None):
         if request.user.is_authenticated:
             if UserInfo.objects.get(user = request.user).last_visit < timezone.now() - timedelta(minutes=30):
-                Response(True)
+                return Response(True)
             else:
                 # Update last visit time after request finished processing.
                 UserInfo.objects.get(user=request.user).update(last_visit=timezone.now())
