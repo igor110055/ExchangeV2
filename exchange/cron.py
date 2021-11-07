53bc811d015c286862647146add39df3fbdb885c
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-07 14:14:50 +0330

    update

--- exchange/cron.py
+++ exchange/cron.py
@@ -18,21 +18,23 @@ def TICKER():
     sleep(2)
 
 def INDEXINFO():
-    end = DT.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ") 
-    start = (DT.datetime.now() - DT.timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%SZ") 
-    print(end)
-    print(start)
-    r = requests.get(f'https://api.nomics.com/v1/currencies/sparkline?key=5f176269caf5ea0dfab684904f9316bf1f4f2bc6&ids=BTC,ETH,TRX,DOGE,USDT&start={start}&end={end}')
+    while i < 5:
+        end = DT.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ") 
+        start = (DT.datetime.now() - DT.timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%SZ") 
+        print(end)
+        print(start)
+        r = requests.get(f'https://api.nomics.com/v1/currencies/sparkline?key=5f176269caf5ea0dfab684904f9316bf1f4f2bc6&ids=BTC,ETH,TRX,DOGE,USDT&start={start}&end={end}')
 
-    sleep(2)
+        sleep(2)
 
-    response = requests.get('https://api.nomics.com/v1/currencies/ticker?key=5f176269caf5ea0dfab684904f9316bf1f4f2bc6&ids=BTC,ETH,TRX,DOGE,USDT')
-    for item in Indexprice.objects.all():
-        item.delete()
-    ind = Indexprice(PriceHistory=r, price = response)
-    ind.save()
+        response = requests.get('https://api.nomics.com/v1/currencies/ticker?key=5f176269caf5ea0dfab684904f9316bf1f4f2bc6&ids=BTC,ETH,TRX,DOGE,USDT')
+        for item in Indexprice.objects.all():
+            item.delete()
+        ind = Indexprice(PriceHistory=r, price = response)
+        ind.save()
 
-    sleep(2)
+        sleep(2)
+        i = i + 1
 
 def RIALTICKER():
         rial = requests.get(url = 'http://api.navasan.tech/latest/?api_key=7RPe7l7pwChjXtZ3vm3xM7vl0xrOoZgk')   
