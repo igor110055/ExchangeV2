cc4f95ae48f28af24947bfa6a552a4cac073ae5c
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-18 17:22:36 +0330

    update

--- exchange/views.py
+++ exchange/views.py
@@ -2042,7 +2042,7 @@ class cp_transfer(APIView):
     permission_classes = [IsAuthenticated]
     def post(self , request):
         coinex = CoinEx(Perpetual.objects.get(user=request.user).apikey, Perpetual.objects.get(user=request.user).secretkey )
-        return Response(coinex.margin_transfer(from_account=request.data['fa'], to_account=request.data['ta'], coin_type=request.data['coin'] , amount=str(request.data['amount'])))
+        return Response(coinex.margin_transfer(from_account=int(request.data['fa']), to_account=int(request.data['ta']), coin_type=request.data['coin'] , amount=str(request.data['amount'])))
 
 class cp_market_order(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication, authentication.TokenAuthentication ]
