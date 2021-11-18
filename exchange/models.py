37d3c995d78659c1328f2738a0e9126487c485be
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-18 14:58:34 +0330

    update

--- exchange/models.py
+++ exchange/models.py
@@ -22,7 +22,7 @@ from django.utils.timezone import utc
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
-    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
+    email_plaintext_message = f"{ROOT}?token={}".format(reverse('password_reset:reset-password-request'), reset_password_token.key)
 
     send_mail(
         # title:
