e9406f37448a329a0efc1e898835eacc7f0a6210
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-20 20:26:04 +0330

    update

--- exchange/management/commands/otp.py
+++ exchange/management/commands/otp.py
@@ -4,10 +4,10 @@ from bit import Key
 from django.core.management.base import BaseCommand, CommandError
 from cryptos import *
 import pyotp
+import base32_lib as base32
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for item in UserInfo.objects.all():
-            if not item.otp:
-                item.otp = pyotp.random_base32()
-                item.save()
+            item.otp = base32.generate(length=10, split_every=5, checksum=True)
+            item.save()
