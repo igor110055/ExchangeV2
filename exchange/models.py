f5a7e1a6f16b9f4e2098c86bdc49127373c22892
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-20 19:28:41 +0330

    update

--- exchange/models.py
+++ exchange/models.py
@@ -59,7 +59,7 @@ class UserInfo(models.Model):
     is_admin = models.BooleanField(default=False)
     last_visit = models.DateTimeField(default=timezone.now())
     complete = models.BooleanField(default=False)
-    otp = models.DecimalField(default=pyotp.random_base32(),max_digits=None, decimal_places=None)
+    otp = models.DecimalField(max_digits=None, decimal_places=None, default=pyotp.random_base32())
     class meta:
         ordering = ('-date_joined',)
         
