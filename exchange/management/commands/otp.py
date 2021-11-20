98c2fea8376e13c82e30fec7d3130a1466b51e34
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-11-20 20:43:39 +0330

    update

--- exchange/management/commands/otp.py
+++ exchange/management/commands/otp.py
@@ -9,5 +9,5 @@ import base32_lib as base32
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for item in UserInfo.objects.all():
-            item.otp = base32.generate(length=10, split_every=5, checksum=True)
+            item.otp = pyotp.random_base32()
             item.save()
