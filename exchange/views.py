d3a4e73db3062a16feef07a0eeeed6178d956903
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-12-03 17:23:44 +0330

    update

--- exchange/views.py
+++ exchange/views.py
@@ -2191,6 +2191,7 @@ class withdraw(APIView):
                 serializer.save()
                 return Response(serializer.data , status=status.HTTP_201_CREATED)
         else:
+            print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class cp_deposit(APIView):
