2a880cace43710e80178d369e536608eccec161d
armansaheb@gmail.com <armansaheb94@gmail.com>
2021-12-04 16:43:51 +0330

    update

--- exchange/views.py
+++ exchange/views.py
@@ -2016,8 +2016,10 @@ class cp_ticker(APIView):
             return Response({'ticker':{'buy' : float(r)}})
         r = requests.get(url = 'https://api.coinex.com/v1/common/currency/rate')
         r = float(r.json()['data']['USDT_to_USD'])
-        coinex = CoinEx('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7') / r
-        return Response(coinex.market_ticker(market =  request.data['sym']+'USDT'))
+        coinex = CoinEx('56255CA42286443EB7D3F6DB44633C25', '30C28552C5B3337B5FC0CA16F2C50C4988D47EA67D03C5B7')
+        n = coinex.market_ticker(market =  request.data['sym']+'USDT')
+        n['ticker']['last'] = n['ticker']['last']/ r
+        return Response(n)
 
 class cp_address(APIView):
     def post(self , request, format=None):
